0818180b93864ade8b9760e26753f4dc71535036
kulkarohan <kulkarohan@gmail.com>
2023-04-23 18:51:42 -0400

    perf: AddressUpgradeable custom errors

--- contracts/utils/AddressUpgradeable.sol
+++ contracts/utils/AddressUpgradeable.sol
@@ -3,6 +3,15 @@
 
 pragma solidity ^0.8.1;
 
+
+error ADDRESS_INSUFFICIENT_BALANCE();
+error ADDRESS_UNABLE_TO_SEND_VALUE();
+error ADDRESS_LOW_LEVEL_CALL_FAILED();
+error ADDRESS_LOW_LEVEL_CALL_WITH_VALUE_FAILED();
+error ADDRESS_INSUFFICIENT_BALANCE_FOR_CALL();
+error ADDRESS_LOW_LEVEL_STATIC_CALL_FAILED();
+error ADDRESS_CALL_TO_NON_CONTRACT();
+
 /**
  * @dev Collection of functions related to the address type
  */
@@ -62,10 +71,13 @@ library AddressUpgradeable {
      * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
      */
     function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
-
+        if (address(this).balance > amount) {
+            revert ADDRESS_INSUFFICIENT_BALANCE();
+        }
         (bool success, ) = recipient.call{value: amount}("");
-        require(success, "Address: unable to send value, recipient may have reverted");
+        if (!success) {
+            revert ADDRESS_UNABLE_TO_SEND_VALUE();
+        }
     }
 
     /**
@@ -87,21 +99,7 @@ library AddressUpgradeable {
      * _Available since v3.1._
      */
     function functionCall(address target, bytes memory data) internal returns (bytes memory) {
-        return functionCallWithValue(target, data, 0, "Address: low-level call failed");
-    }
-
-    /**
-     * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`], but with
-     * `errorMessage` as a fallback revert reason when `target` reverts.
-     *
-     * _Available since v3.1._
-     */
-    function functionCall(
-        address target,
-        bytes memory data,
-        string memory errorMessage
-    ) internal returns (bytes memory) {
-        return functionCallWithValue(target, data, 0, errorMessage);
+        return functionCallWithValue(target, data, 0);
     }
 
     /**
@@ -115,25 +113,16 @@ library AddressUpgradeable {
      *
      * _Available since v3.1._
      */
-    function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) {
-        return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
-    }
-
-    /**
-     * @dev Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
-     * with `errorMessage` as a fallback revert reason when `target` reverts.
-     *
-     * _Available since v3.1._
-     */
     function functionCallWithValue(
         address target,
         bytes memory data,
-        uint256 value,
-        string memory errorMessage
+        uint256 value
     ) internal returns (bytes memory) {
-        require(address(this).balance >= value, "Address: insufficient balance for call");
+        if (address(this).balance < value) {
+            revert ADDRESS_INSUFFICIENT_BALANCE();
+        }
         (bool success, bytes memory returndata) = target.call{value: value}(data);
-        return verifyCallResultFromTarget(target, success, returndata, errorMessage);
+        return verifyCallResultFromTarget(target, success, returndata);
     }
 
     /**
@@ -142,23 +131,12 @@ library AddressUpgradeable {
      *
      * _Available since v3.3._
      */
-    function functionStaticCall(address target, bytes memory data) internal view returns (bytes memory) {
-        return functionStaticCall(target, data, "Address: low-level static call failed");
-    }
-
-    /**
-     * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
-     * but performing a static call.
-     *
-     * _Available since v3.3._
-     */
     function functionStaticCall(
         address target,
-        bytes memory data,
-        string memory errorMessage
+        bytes memory data
     ) internal view returns (bytes memory) {
         (bool success, bytes memory returndata) = target.staticcall(data);
-        return verifyCallResultFromTarget(target, success, returndata, errorMessage);
+        return verifyCallResultFromTarget(target, success, returndata);
     }
 
     /**
@@ -170,18 +148,19 @@ library AddressUpgradeable {
     function verifyCallResultFromTarget(
         address target,
         bool success,
-        bytes memory returndata,
-        string memory errorMessage
+        bytes memory returndata
     ) internal view returns (bytes memory) {
         if (success) {
             if (returndata.length == 0) {
                 // only check isContract if the call was successful and the return data is empty
                 // otherwise we already know that it was a contract
-                require(isContract(target), "Address: call to non-contract");
+                if (!isContract(target)) {
+                    revert ADDRESS_CALL_TO_NON_CONTRACT();
+                }
             }
             return returndata;
         } else {
-            _revert(returndata, errorMessage);
+            _revert(returndata);
         }
     }
 
@@ -193,17 +172,16 @@ library AddressUpgradeable {
      */
     function verifyCallResult(
         bool success,
-        bytes memory returndata,
-        string memory errorMessage
+        bytes memory returndata
     ) internal pure returns (bytes memory) {
         if (success) {
             return returndata;
         } else {
-            _revert(returndata, errorMessage);
+            _revert(returndata);
         }
     }
 
-    function _revert(bytes memory returndata, string memory errorMessage) private pure {
+    function _revert(bytes memory returndata) private pure {
         // Look for revert reason and bubble it up if present
         if (returndata.length > 0) {
             // The easiest way to bubble the revert reason is using memory via assembly
@@ -213,7 +191,7 @@ library AddressUpgradeable {
                 revert(add(32, returndata), returndata_size)
             }
         } else {
-            revert(errorMessage);
+            revert ADDRESS_LOW_LEVEL_CALL_FAILED();
         }
     }
 }
